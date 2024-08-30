08d2067913b3aa46dd5e8228fcfa290224150825
mlalday <marco_alday@hotmail.com>
2024-08-29 21:32:02 -0300

    fix

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import Link from "next/link"
 import { useRouter } from "next/router"
-import {Bounce, ToastContainer, toas} from 'react-toastify'
+import {Bounce, ToastContainer, toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 export default function register(){
@@ -9,13 +9,31 @@ export default function register(){
     //variaveis de tela
     const [name, setName] = useState('')
     const [pass, setPass] = useState('')
+    const router = useRouter()
 
     //conexão com backend
     const handleLogin = async (e:any) => {
         e.preventDefault()
-        const response = await fetch('https://apl-back-educablog-1.onrender.com/')
-    }
+        const response = await fetch('https://apl-back-educablog-1.onrender.com/autenticacao/signin',
+          {
+            method:'POST',
+            headers:{
+              'Content-Type': 'application;json', 
+            },
+            body: JSON.stringify({name, pass}),
 
+          }
+        )
+        if(response.ok){
+          router.push('/')
+        } else {
+          toast.error(`Erro ao Registrar Usuario`,{
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: true
+          })
+        }
+    }
 
     return (
     <main className="flex flex-1 flex-col h-screen w-screen justify-center items-center bg-background-900" >
@@ -27,10 +45,26 @@ export default function register(){
        <input className="mt-2 font-sans text-lg text-fontColor-900 bg-background-800 border border-fontColor-900 rounded-xl p-2 w-5/12" type= "text" placeholder="Senha" onChange={(e)=> setPass(e.target.value)}/>
        <div className="flex flex-1 flex-row w-[40%] max-h-12  mt-6 justify-between ml-auto mr-auto item-center" >
         <button className="bg-background-800 p-2 rounded-xl text-fontColor-800 text-lg font-bold border border-fontColor-800 hover:bg-background-900 hover:border hover:border-fontColor-900 hover:text-fontColor-800 w-64"> Cadastrar</button>
-        <button disabled={(pass=='' || name == '')} className="bg-background-500 p-2 rounded-xl text-fontColor-200 text-lg font-bold hover:bg-background-900 hover:border border-fontColor-900 hover:text-fontColor-800 w-64"> Login</button>
+        <button onClick={handleLogin} disabled={(pass=='' || name == '')} className="bg-background-500 p-2 rounded-xl text-fontColor-200 text-lg font-bold hover:bg-background-900 hover:border border-fontColor-900 hover:text-fontColor-800 w-64"> Login</button>
        </div>
        <Link className= "mt-1 hover:font-bold mr-72" href="/"> Voltar </Link>
       </div>
+
+      <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"          
+          transition={Bounce}
+          />
+
+
     </main>
     )
 }
